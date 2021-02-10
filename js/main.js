4a9b99aa748a9825836927d7538c7cb13cfa22ae
Almog <almoglemberg@gmail.com>
2021-02-10 17:42:04 +0200

    add input text and clear options added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 var gElCanvas;
 var gCtx;
+var gCurrImgId = 3;
 
 function init() {
     gElCanvas = document.getElementById('meme-canvas');
     gCtx = gElCanvas.getContext('2d');
-    drawImg(3)
+    drawImg(gCurrImgId);
+    addEventListeners();
 }
 
+function addEventListeners() {
+    var elInput = document.getElementById('input-text');
+    elInput.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            document.getElementById("add-text-btn").click();
+        }
+    });
+}
+
+function onAddText() {
+    var elInput = document.getElementById('input-text');
+    var text = elInput.value;
+    drawText(text);
+    elInput.value = '';
+}
 
 function drawImg(id) {
     const img = new Image();
@@ -17,12 +34,18 @@ function drawImg(id) {
     }
 }
 
-function drawText(meme, x = gElCanvas.width / 2, y = 25) {
+function drawText(text, x = gElCanvas.width / 2, y = 30) {
     gCtx.lineWidth = 2
     gCtx.strokeStyle = 'black'
     gCtx.fillStyle = 'white'
     gCtx.font = '30px impact'
     gCtx.textAlign = 'center'
-    gCtx.fillText(meme.lines[0].txt, x, y)
-    gCtx.strokeText(meme.lines[0].txt, x, y)
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+function onClearCanvas() {
+    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
+    drawImg(gCurrImgId);
+
 }
